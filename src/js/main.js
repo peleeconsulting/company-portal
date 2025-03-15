6b013701abfb68b9fc9b8984a26d7a6df5efa460
Mohaimen Khan <mohaimenkhan@microsoft.com>
2025-03-15 14:56:22 -0700

    update to have Solutions over Consulting

--- src/js/main.js
+++ src/js/main.js
@@ -19,7 +19,7 @@ const initHeader = () => {
   header.innerHTML = `
     <div class="container">
       <nav class="navbar navbar-expand-lg">
-        <a class="navbar-brand" href="index.html">6ixStack <span class="text-primary">Consulting</span></a>
+        <a class="navbar-brand" href="index.html">6ixStack <span class="text-primary">Solutions</span></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
